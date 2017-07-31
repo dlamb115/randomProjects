6093f05527c54e2275edee378695aff731ab8eb2
Denise Lamb <dlamb115@gmail.com>
2017-07-31 11:34:39 -0400

    some stuff

--- high_low.py
+++ high_low.py
@@ -1,7 +1,7 @@
 # 7/20/2017 Denise Lamb
 # so i attended a 6 hour General Assembly class in Boston 
 # and this is all they taught us to make. We didn't even cover python until
-# after lunch. Argh! #wantToLearnMore!!
+# after lunch. Argh! #wantToLearnMore
 
 # this script runs a game of high/low using python interpreter 3.x 
 
